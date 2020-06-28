3584ede2482030cced2acba68f99a1ac4a5a5b16
scoiak <kaiocs@live.com>
2020-06-28 15:44:49 -0300

    First

---
+++ README.md
@@ -0,0 +1,49 @@
+# BthTools
+
+[![Build Status](https://github.com/scoiak/bthtools](https://github.com/scoiak/bthtools)
+
+# Instalação
+```
+pip3 install bthtools
+```
+
+## Atualização
+Obtém a versão mais atual da biblioteca.
+```
+pip3 install --upgrade bthtools
+```
+
+# Recursos
+`BthTools` é uma biblioteca que oferece recursos para integração de sistemas da suíte Betha Cloud com aplicações
+desenvolvidas em python.
+
+* Leitura de Fontes de Dados
+* Verificação e validação de Tokens
+* Interação com Service Layer
+
+# Utilização
+
+O primeiro passo para utilização da biblioteca é realizar a importação no arquivo do projeto. Para isso, basta inserir
+a seguinte linha de código no início do projeto:
+
+``` python
+from bthtools import *
+```
+
+Feito isso, já é possível instanciar as classes da biblioteca e começar a utiliza-las.
+
+# Token
+
+Para utilizar qualquer uma das classes de consulta, é necessário apontar para a classe o token de serviço que dará
+autorização para utilizar os serviços Betha. Segue exemplo abaixo de como setar o valor do token na classe:
+
+ ``` python
+ # Importando bliblioteca 'bthtools'
+from bthtools import *
+
+ # Criando instância da classe 'BthFontesDados'
+bth = BthFontesDados()
+
+# Setando o token de serviço na classe para utilização dos métodos de consulta
+bth.setToken('034453a8-df56-4606-bd1f-8403518f7199')
+```
---
+++ bthtools.py
@@ -0,0 +1,129 @@
+import json
+import requests
+import re
+from dotmap import DotMap
+
+
+class BthFontesDados:
+    # Constructor (Seta valores iniciais de parâmetros da classe)
+    def __init__(self):
+        self.p_token = ''
+        self.p_timeout = 2000
+        self.p_limit = 100
+        self.dadosToken = {}
+        self.dadosEntidade = {}
+        self.useDotmap = True
+
+    # Retorna o valor do token em uso
+    def getToken(self):
+        return self.p_token
+
+    # Seta o valor para token genérico da classe
+    def setToken(self, n):
+        self.p_token = n
+        self.dadosToken = self.gerarDadosToken(n)
+
+    # Seta valor de timeout das requisições
+    def setTimeout(self, n):
+        self.p_timeout = n
+
+    # Seta valor de limite de dados (paginação) das requisições
+    def setLimit(self, n):
+        self.p_limit = n
+
+    # Define se os resultados das requisições serão retornados com uso do DotMap
+    def setDotmap(self, n):
+        self.useDotmap = n
+
+    # Retorna o valor dos dados da entidade
+    def getDadosEntidade(self):
+        if self.dadosEntidade == {}:
+            self.gerarDadosEntidade()
+        return self.dadosEntidade
+
+    def getSelf(self):
+        return self
+
+    # Busca endereço da fonte desejado no JSON local de ativos
+    def getUrlFonteBetha(self, args):
+        r = {'status': 0, 'url': ''}
+        try:
+            with open('bthDS.json') as json_file:
+                data = json.load(json_file)
+                for i in data:
+                    if i['identificador'] == args['sistema']:
+                        for j in i['ativos']:
+                            if j['tema'] == args['fonte'] and j['tipoOperacao']['value'] == 'L':
+                                if 'hostExterno' in i and 'endereco' in j:
+                                    r = {'status': 1, 'url': (i['hostExterno'] + j['endereco'])}
+                                break
+        except:
+            print('Falha ao executar função "getUrlFonteBetha"')
+        return r
+
+    # Consulta dados do token ativo
+    def gerarDadosToken(self, token):
+        r = {'status': 0}
+        try:
+            url = "https://oauth.cloud.betha.com.br/auth/oauth2/tokeninfo"
+            params = {'access_token': self.p_token}
+            r = requests.get(url=url, params=params)
+            data = r.json()
+            r = {
+                 'status': 1,
+                 'expired': re.search("(?<=expired\'\: )(False|True)", str(data)).group(),
+                 'database': re.search("(?<=databaseId\" \: \")(\d+)(?!=\")", str(data)).group(),
+                 'entity': re.search("(?<=entityId\" \: \")(\d+)(?!=\")", str(data)).group()
+                 }
+        except:
+            print(f'Falha ao consultar dados para o token {self.p_token}')
+        return r
+
+    # Busca informações da entidade vinculado ao token
+    def gerarDadosEntidade(self):
+        url = f'https://contabilidade-fontes-dados.cloud.betha.com.br/contabilidade/fontes-dados/contabil/entidades-ctb?fields=id, nome, cnpj, descricaoNatureza&filter=id={self.dadosToken["entity"]}'
+        headers = {'authorization': f'bearer {self.p_token}'}
+        r = requests.get(url=url, headers=headers, timeout=self.p_timeout)
+        data = r.json()['content']
+        self.dadosEntidade = {
+            'id': data[0]['id'],
+            'nome': data[0]['nome'],
+            'cnpj': data[0]['cnpj'],
+            'natureza': data[0]['descricaoNatureza']
+        }
+
+    # Busca os dados na fonte conforme parâmetros de entrada
+    def busca(self, args):
+        retBusca = []
+        hasNext = True
+        url = self.getUrlFonteBetha({'sistema': args['sistema'], 'fonte': args['fonte']})['url']
+        params = {'offset': 0, 'limit': self.p_limit}
+        if 'token' in args:
+            headers = {'authorization': f'bearer {args["token"]}'}
+        else:
+            if self.p_token == '':
+                print('Não foi especificado um token .')
+                return {}
+            else:
+                headers = {'authorization': f'bearer {self.p_token}'}
+        if 'campos' in args:
+            params['fields'] = args['campos']
+        if 'criterio' in args:
+            params['filter'] = args['criterio']
+        if 'ordenacao' in args:
+            params['sort'] = args['ordenacao']
+        if 'parametros' in args:
+            for p in args['parametros']:
+                url = url.replace("{" + p + "}", str(args['parametros'][p]))
+        while hasNext:
+            r = requests.get(url=url, params=params, headers=headers, timeout=self.p_timeout)
+            data = r.json()
+            if 'content' in data:
+                for i in data['content']:
+                    if self.useDotmap:
+                        retBusca.append(DotMap(i))
+                    else:
+                        retBusca.append(i)
+            params['offset'] += self.p_limit
+            hasNext = str(data['hasNext']) == 'True'
+        return retBusca
